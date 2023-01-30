fff9fa16ce68723f82154423b0623bd64b799fde
Anita Sri Handayani <anita.handa26@gmail.com>
2023-01-30 11:17:45 +0700

    update login comp

--- src/components/loginComp.vue
+++ src/components/loginComp.vue
@@ -22,6 +22,10 @@
                         required="true">
                     </text-input>
 
+                    <hr/>
+
+                    Email: {{ email }}
+
                     <hr>
                     <input type="submit" class="btn btn-primary" value="Login">
                 </form-tag>
@@ -49,6 +53,26 @@ export default {
     methods: {
         submitHandler() {
             console.log("submitHandler called - success!");
+
+            const payload = {
+                email: this.email,
+                password: this.password,
+            }
+
+            const requestOptions = {
+                method: "POST",
+                body: JSON.stringify(payload),
+            }
+
+            fetch("http://localhost:8081/users/login", requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log("Error:", data.message);
+                } else {
+                    console.log(data);
+                }
+            })
         }
     },
 }
