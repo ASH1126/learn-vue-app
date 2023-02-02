6c428121a020c32e64644beab6dc327dafaefeb2
Anita Sri Handayani <anita.handa26@gmail.com>
2023-02-02 11:32:05 +0700

    add cookie

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@ import Header from "./components/headerComp.vue";
 import Footer from "./components/footerComp.vue";
 import { store } from "./components/store.js";
 
+const getCookie = (name) => {
+  return document.cookie.split("; ").reduce((r, v) => {
+    const parts = v.split("=");
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+  }, "");
+}
+
 export default {
   name: "App",
   components: {
@@ -22,6 +29,23 @@ export default {
       store,
     };
   },
+  beforeMount() {
+    // check for a cookie
+    let data = getCookie("_site_data");
+
+    if (data !== "") {
+      let cookieData = JSON.parse(data);
+
+      // update store
+      store.token = cookieData.token.token;
+      store.user = {
+        id: cookieData.user.id,
+        first_name: cookieData.user.first_name,
+        last_name: cookieData.user.last_name,
+        email: cookieData.user.email,
+      }
+    }
+  }
 };
 </script>
 
